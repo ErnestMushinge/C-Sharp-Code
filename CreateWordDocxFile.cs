928cf0a6915f628a56eacb57006f922571b734a4
Ernest Mushinge <emushinge@gmail.com>
2019-06-24 16:53:37 -0400

    Clean code and formatting
    - Cleaned code
    - Applied formatting

--- CreateWordDocxFile.cs
+++ CreateWordDocxFile.cs
@@ -14,26 +14,25 @@ namespace DynamicDocx
         Ernest Mushinge: This class works an abstruction layer (Wrapper) with another library class that is not published
         */
         
-        
         //Create DocX
         public static void CreatDocX(string filepath, out WordprocessingDocument docx)
         {
             CU_DOCX.CreateDoc(filepath, out docx, false);
             //Register default style to the created document
             RegisterStyle(docx);
-        }//Create DocX return obj ends
+        }
 
         //SaveDoc
         public static void SaveDoc(WordprocessingDocument wordDoc)
         {
             wordDoc.MainDocumentPart.Document.Save();
             wordDoc.Close();
-        }//save docx Ends
+        }
 
         public void OpenDocX(string filepath, out WordprocessingDocument docx)
         {
             docx = WordprocessingDocument.Open(filepath, true);
-        }//OpenDocX ends
+        }
 
         //Write text to docx with your style using docx object
         public static void WriteTextWithYourStyle(WordprocessingDocument docx, string text, string StyleName, JustificationValues textAlignment, string fontSize = "24", string fontColor = "black", bool isBold = false, bool isUnderLine = false, bool isItalic = false)
@@ -43,7 +42,7 @@ namespace DynamicDocx
             CU_DOCX.AddNewPStyle(docx, StyleName, StyleName, newStyleProperty);
             CU_DOCX.WriteParagraph(docx, CU_DOCX.MakeParagraph(StyleName, textAlignment, CU_DOCX.MakeRun(text, "newStyleName")));
 
-        }//WriteTextWithStyle ends
+        }
 
         //Write image to docx with alignment using docx object, this subroutine takes in image bytes from the stream
         public static void WriteImage(WordprocessingDocument docx, string RegisteredStyleName, Byte[] ImageBytes, JustificationValues alignment)
@@ -51,7 +50,7 @@ namespace DynamicDocx
             string picID = CU_DOCX.InsertPic2DocLib(docx, new MemoryStream(ImageBytes), ImagePartType.Gif);
             Run TestMolImage = CU_DOCX.AddImageAsRun(picID);
             CU_DOCX.WriteParagraph(docx, CU_DOCX.MakeParagraph(RegisteredStyleName, alignment, TestMolImage));
-        }//Write image to docx with alignment ends
+        }
 
         //serialize the image. Subroutine for images on disk to pass in a MemoryStream: Ernest used this subroutin for testing image on disk
         public static byte[] SerializeImage(string picPath)
@@ -65,10 +64,10 @@ namespace DynamicDocx
                     image.Save(m, image.RawFormat);
                     imageBytes = new byte[m.Length];
                     imageBytes = m.ToArray();
-                }//end using
-            }//end using
+                }
+            }
             return imageBytes;
-        }//SerializeImage
+        }
 
         //Create a line to docx object
         public static void CreateLine(WordprocessingDocument doc)
@@ -77,13 +76,13 @@ namespace DynamicDocx
             CU_DOCX.ToggleTableCellBorders(tableCell, bottom: true);
             Table tline_Alert = CU_DOCX.MakeTable(BorderValues.None, true, CU_DOCX.MakeTableRow(tableCell));
             CU_DOCX.WriteTable(doc, tline_Alert);
-        }//CreatLine ends
+        }
 
         //Create Space/tab in docx object
         public static void CreateSpace(WordprocessingDocument doc)
         {
             CU_DOCX.WriteParagraph(doc, CU_DOCX.MakeParagraph("S00", JustificationValues.Left, CU_DOCX.MakeRun("")));
-        }//Create Space ends
+        }
 
         //Ernest Mushinge; This subroutine will put data in the Row and Cells using a docX object
         public static void CreateTableAndInsertDataInRowsAndCellsDocx(WordprocessingDocument doc, string[] DataForRow, string[] DataForCells, string StyleName, string fontSize = "24", string fontColor = "black", String CellColor = "white", bool isBold = false, bool isUnderLine = false, bool isItalic = false)
@@ -99,7 +98,6 @@ namespace DynamicDocx
             {
                 if (DataForRow.Length == 0) return;
                 tbRow[k] = CU_DOCX.MakeTableCell(CU_DOCX.MakeParagraph("S0B", JustificationValues.Left, CU_DOCX.MakeRun(DataForRow[k], StyleName)));
-                /////////////////////////////////////////////////////
                 //The code below is for shading/color the row of a table
                 var tcp = new TableCellProperties(new TableCellWidth()
                 {
@@ -115,9 +113,8 @@ namespace DynamicDocx
                 };
                 tcp.Append(shading);
                 tbRow[k].Append(tcp);
-                ////////////////////////////////////////////////
                 CU_DOCX.ToggleTableCellBorders(tbRow[k], 0, true, true, true, true);
-            }//foreach ends
+            }
 
             for (int v = 0; v < DataForCells.Length; v++)
             {
@@ -125,7 +122,7 @@ namespace DynamicDocx
                 tbCell[v] = CU_DOCX.MakeTableCell(CU_DOCX.MakeParagraph(StyleName, JustificationValues.Left, CU_DOCX.MakeRun(DataForCells[v], StyleName)));
 
                 CU_DOCX.ToggleTableCellBorders(tbCell[v], 0, true, true, true, true);
-            }//for loop end
+            }
 
             TableRow TableRow = CU_DOCX.MakeTableRow(tbRow);
             //ThisRow is for cells data
@@ -136,8 +133,7 @@ namespace DynamicDocx
             // Table table2 = CU_DOCX.MakeTable(BorderValues.None, true, TableCell2);
             CU_DOCX.WriteTable(doc, tableRow);
             CU_DOCX.WriteTable(doc, tableCell);
-
-        }//InsertDataInRowsAndCells using docX object end
+        }
 
         //WriteImage; This subroutine will put data in the Row and Cells using a docX object
         public static void CreateTableWithImageDocx(WordprocessingDocument doc, string[] DataForRow, string[] DataForCells, string StyleName, JustificationValues position, bool DoYouWantBorders = true, string BorderColor = "228B22", string fontSize = "24", string fontColor = "black", bool isBold = false, bool isUnderLine = false, bool isItalic = false)
@@ -170,8 +166,8 @@ namespace DynamicDocx
                     if (DoYouWantBorders)
                         // CU_DOCX.ToggleTableCellBorders(tbRow[k], 0, true, true, true, true);
                         ToggleTableCellBordersWithColor(tbRow[k], 0, BorderColor, true, true, true, true);
-                }//else ends
-            }//forloop ends for Row (content for row)
+                }
+            }
 
             //loop through array containng data data for cells
             for (int v = 0; v < DataForRow.Length; v++)
@@ -192,8 +188,8 @@ namespace DynamicDocx
                     if (DoYouWantBorders)
                         // CU_DOCX.ToggleTableCellBorders(tbCell[v], 0, true, true, true, true);
                         ToggleTableCellBordersWithColor(tbCell[v], 0, BorderColor, true, true, true, true);
-                }//else ends
-            }//for loop end
+                }
+            }
 
             //This Table Row is for Header row
             TableRow TableRow = CU_DOCX.MakeTableRow(tbRow);
@@ -205,8 +201,7 @@ namespace DynamicDocx
 
             CU_DOCX.WriteTable(doc, tableRow);
             CU_DOCX.WriteTable(doc, tableCell);
-
-        }//InsertDataInRowsAndCells using docX object end
+        }
 
         //This subroutine will write an InLine Sentence
         public static void InlineSentence(WordprocessingDocument docx, string inLineTitle, string inLineTitleChild, JustificationValues values)
@@ -214,7 +209,7 @@ namespace DynamicDocx
             Run r1 = CU_DOCX.MakeRun(inLineTitle, "C0B");
             Run r2 = CU_DOCX.MakeRun(inLineTitleChild, "S01");
             CU_DOCX.WriteParagraph(docx, CU_DOCX.MakeParagraph("S00", values, r1, r2));
-        }//inLineParagraph ends
+        }
 
         //This subroutine will customize a table to put paddings inside the cells.For Exampl, image in the cells never used to have padding
         private static Table MakeTable(BorderValues bvV = BorderValues.None, bool FullWidth = false, string tableMargin = "10", params TableRow[] trs)
@@ -224,9 +219,7 @@ namespace DynamicDocx
             TableProperties tblProp = new TableProperties(
 
                 new TableBorders(
-
                     new TopBorder() { Val = new EnumValue<BorderValues>(bvV), Size = 24 },
-
                     // new TopBorder() { Color = "FFFF00" },
                     new BottomBorder() { Val = new EnumValue<BorderValues>(bvV), Size = 24 },
                     new LeftBorder() { Val = new EnumValue<BorderValues>(bvV), Size = 24 },
@@ -242,7 +235,6 @@ namespace DynamicDocx
                 tblProp.Append(new TableWidth() { Type = TableWidthUnitValues.Dxa, Width = "9000" });
 
             //manage deafult table margins
-
             TableCellMarginDefault m = new TableCellMarginDefault();
             m.TableCellLeftMargin = new TableCellLeftMargin() { Width = 100, Type = TableWidthValues.Dxa };
             m.TableCellRightMargin = new TableCellRightMargin() { Width = 100, Type = TableWidthValues.Dxa };
@@ -258,15 +250,14 @@ namespace DynamicDocx
 
             for (int i = 0; i < trs.Length; i++)
                 table.Append(trs[i]);
-
-            return table;
-        }//Make table ends
+                return table;
+        }
 
         //This subroutine will allow changing border color of a table
         private static void ToggleTableCellBordersWithColor(TableCell tc, Int16 custWidth = 0, string BorderColor = "", bool left = false, bool right = false, bool top = false, bool bottom = false, TableVerticalAlignmentValues Vali = TableVerticalAlignmentValues.Top)
         {//adds/removes thin borders to the table cell, can also reset cellwidth in Dxa units: 1440 = 1 inch
             if (tc == null) return;
-
+            
             TableCellProperties tcProps = new TableCellProperties();
 
             if (custWidth > 0)
@@ -286,12 +277,11 @@ namespace DynamicDocx
             tcProps.TableCellBorders = tcBds;               //tcProps.Append(tcBds);
             tc.TableCellProperties = tcProps;               //tc.Append(tcProps);            
 
-        }//ToggleTableCell Ends
+        }
 
         //Ernest; This subroutine will automatically register Style to the document
         public static void RegisterStyle(WordprocessingDocument doc)
         {
-
             CU_DOCX.AddNewPStyle(doc, "S00", "S00", CU_DOCX.MakeStyleRunProps("Segoe UI", "24", ""));                     //default
             CU_DOCX.AddNewPStyle(doc, "S01", "S01", CU_DOCX.MakeStyleRunProps("Segoe UI", "24", "808080"));               //shaded
             CU_DOCX.AddNewPStyle(doc, "S02", "S02", CU_DOCX.MakeStyleRunProps("Segoe UI", "24", "FF0000", B: true));      //red
@@ -313,8 +303,7 @@ namespace DynamicDocx
             CU_DOCX.AddNewCStyle(doc, "C0U", "C0U", "S0U", CU_DOCX.MakeStyleRunProps(U: true));
             CU_DOCX.AddNewCStyle(doc, "C0I", "C0I", "S0I", CU_DOCX.MakeStyleRunProps(I: true));
 
-        }//Register style ends 
-
+        }
 
     }//End of DynamicDocx
 }//End of HTML_to_DOCX
